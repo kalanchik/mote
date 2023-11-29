994a78373ab2b2078264d7985c0136cebfd21d7a
kalanchik <61646943+kalanchik@users.noreply.github.com>
2023-11-29 13:25:22 +0300

    start meet backend

--- mote_app/lib/screens/main_screens/meets_screen/bloc/meets_bloc.dart
+++ mote_app/lib/screens/main_screens/meets_screen/bloc/meets_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mote_app/models/meets_models.dart';
-import 'package:mote_app/models/test_users.dart';
 
 part 'meets_event.dart';
 part 'meets_state.dart';
@@ -10,23 +9,22 @@ class MeetsBloc extends Bloc<MeetsEvent, MeetsState> {
   MeetsBloc() : super(MeetsInitial()) {
     on<MeetsLoadingMeets>((event, emit) async {
       emit(MeetsLoadingMeetsState());
-      await Future.delayed(const Duration(seconds: 2));
       emit(MeetsLoadedMeetsState(
-        myMeetsList: TestUsers.myMeetsList,
-        findMeetsList: TestUsers.findMeetsList,
+        myMeetsList: [],
+        findMeetsList: [],
       ));
     });
     on<MeetsCreateMeetEvent>((event, emit) {
-      emit(MeetsUploadPhoto(meet: event.meet));
+      // emit(MeetsUploadPhoto(meet: event.meet));
     });
     on<MeetsSaveMeetsEvent>((event, emit) async {
-      emit(MeetsCreatingMeet());
-      event.meet.addNewUser(user: TestUsers.anna);
-      event.meet.addNewUser(user: TestUsers.zlata);
-      TestUsers.myMeetsList.add(event.meet);
-      TestUsers.findMeetsList.add(event.meet);
-      await Future.delayed(const Duration(seconds: 2));
-      emit(MeetsCreatedMeet());
+      // emit(MeetsCreatingMeet());
+      // event.meet.addNewUser(user: TestUsers.anna);
+      // event.meet.addNewUser(user: TestUsers.zlata);
+      // TestUsers.myMeetsList.add(event.meet);
+      // TestUsers.findMeetsList.add(event.meet);
+      // await Future.delayed(const Duration(seconds: 2));
+      // emit(MeetsCreatedMeet());
     });
   }
 }
