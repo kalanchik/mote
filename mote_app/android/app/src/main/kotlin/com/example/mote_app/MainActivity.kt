8d6c8fc0a4ec747b1a5eeab76246aee4d40bf2ee
kalanchik <61646943+kalanchik@users.noreply.github.com>
2023-11-29 11:59:02 +0300

    add mobile app

---
+++ mote_app/android/app/src/main/kotlin/com/example/mote_app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mote_app
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
+        MapKitFactory.setApiKey("2c944320-614e-49aa-8c9d-f546f36262b7") // Your generated API key
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
